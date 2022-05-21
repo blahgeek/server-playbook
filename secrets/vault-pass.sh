bc51d90b26733aa80135f69d1f64f4f4c2c94147
Yikai Zhao <yikai@z1k.dev>
2022-05-21 16:14:07 +0800

    eastwatch as the ipv6 exit of wall

--- secrets/vault-pass.sh
+++ secrets/vault-pass.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+GPG=gpg2
+if ! which $GPG > /dev/null 2>&1; then
+    GPG=gpg
+fi
+
 echo "Getting vault pass from gpg..." 1>&2
 cd "$(dirname "$0")"
-gpg2 --quiet --batch --use-agent --decrypt ./password.asc
+$GPG --quiet --batch --use-agent --decrypt ./password.asc
